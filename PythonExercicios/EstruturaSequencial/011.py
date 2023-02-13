09577fb94888780209392d6873f9174e78f103b3
Carlos Leonardo <c.carlos-bp@hotmail.com>
2023-02-12 23:04:20 -0300

    2 number int and 1 number real

---
+++ PythonExercicios/EstruturaSequencial/011.py
@@ -0,0 +1,7 @@
+a = int(input('Informe o primeiro número inteiro: '))
+b = int(input('Informe o segundo número inteiro: '))
+c = float(input('Informe um número real: '))
+print('O produto do dobro do primeiro com metade do segundo é: {}'.format(
+    (a * 2) + (b / 2)))
+print('A soma do triplo do primeiro com o terceiro é: {}'.format((a * 3) + c))
+print('O terceiro elevado ao cubo é: {}'.format(c ** 3))
