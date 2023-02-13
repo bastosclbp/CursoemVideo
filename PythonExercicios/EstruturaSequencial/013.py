b00fdb7e48aebedf1b59c851e457f9c41c9b5ad1
Carlos Leonardo <c.carlos-bp@hotmail.com>
2023-02-13 20:40:08 -0300

    To EN

--- PythonExercicios/EstruturaSequencial/013.py
+++ PythonExercicios/EstruturaSequencial/013.py
@@ -1,5 +1,5 @@
 height = float(input('Informe sua altura: '))
-homem = (72 * height) - 58
-mulher = (62.1 * height) - 44.7
-print('Seu peso ideal se for homem é: {:.2f}Kg'.format(homem))
-print('Seu peso ideal se for mulher é: {:.2f}Kg'.format(mulher))
+man = (72 * height) - 58
+woman = (62.1 * height) - 44.7
+print('Seu peso ideal se for homem é: {:.2f}Kg'.format(man))
+print('Seu peso ideal se for mulher é: {:.2f}Kg'.format(woman))
