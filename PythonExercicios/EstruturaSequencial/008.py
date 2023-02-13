99ae9931e779a478ce9fc278e2db05f250e16c65
Carlos Leonardo <c.carlos-bp@hotmail.com>
2023-02-12 22:36:54 -0300

    exclusão de variável

--- PythonExercicios/EstruturaSequencial/008.py
+++ PythonExercicios/EstruturaSequencial/008.py
@@ -1,4 +1,3 @@
 valor = float(input('Quanto você ganha por hora? '))
 horas = float(input('Quantas horas você trabalhou no mês? '))
-mes = (valor * horas)
-print('O total do seu salário no mês é de: R${}'.format(mes))
+print('O total do seu salário no mês é de: R${}'.format(valor * horas))
