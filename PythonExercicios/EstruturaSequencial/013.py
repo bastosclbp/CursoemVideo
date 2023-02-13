2cfe7008bae2822114d83aeab66b940208cb5583
Carlos Leonardo <c.carlos-bp@hotmail.com>
2023-02-13 20:37:50 -0300

    Peso ideal para homem e mulher

---
+++ PythonExercicios/EstruturaSequencial/013.py
@@ -0,0 +1,5 @@
+height = float(input('Informe sua altura: '))
+homem = (72 * height) - 58
+mulher = (62.1 * height) - 44.7
+print('Seu peso ideal se for homem é: {:.2f}Kg'.format(homem))
+print('Seu peso ideal se for mulher é: {:.2f}Kg'.format(mulher))
