db4533db0ec5f6bbd9ec2283955653c4d47095be
Carlos Leonardo <c.carlos-bp@hotmail.com>
2023-02-23 20:54:26 -0300

    Separando dígitos de um número (com matemática)

--- PythonExercicios/desafio023.py
+++ PythonExercicios/desafio023.py
@@ -1,5 +1,9 @@
-numero = input('Digite um número de 0 a 9999: ')
-print('Unidade: {}'.format(numero[3:4]))
-print('Dezena: {}'.format(numero[2:3]))
-print('Centena: {}'.format(numero[1:2]))
-print('Milhar: {}'.format(numero[0:1]))
+numero = int(input('Digite um número de 0 a 9999: '))
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+print('Unidade: {}'.format(u))
+print('Dezena: {}'.format(d))
+print('Centena: {}'.format(c))
+print('Milhar: {}'.format(m))
