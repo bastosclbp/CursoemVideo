c54dc96a4bbdb48b5d6dc1d50e1c64d7db92c585
Carlos Leonardo <c.carlos-bp@hotmail.com>
2023-02-23 20:59:34 -0300

    Verificando as primeiras letras de um texto

--- PythonExercicios/desafio024.py
+++ PythonExercicios/desafio024.py
@@ -1,2 +1,2 @@
-cidade = str(input('Digite o nome da cidade: '))
-print('SANTO' in cidade[0:5].upper())
+cidade = str(input('Digite o nome da cidade: ')).strip()
+print('SANTO' in cidade[:5].upper())
