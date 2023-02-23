0979893b33a2b4d8400866afac11add70531a4dc
Carlos Leonardo <c.carlos-bp@hotmail.com>
2023-02-23 20:46:29 -0300

    Analisador de Textos

--- PythonExercicios/desafio022.py
+++ PythonExercicios/desafio022.py
@@ -1,6 +1,7 @@
-nome = input('Informe seu nome: ')
+nome = input('Informe seu nome: ')  # .strip()
 print(nome.upper())
 print(nome.lower())
 print(len(''.join(nome.split())))
+# print(len(nome) - nome.count(' ')))
 dividido = nome.split()
 print(len(dividido[0]))
