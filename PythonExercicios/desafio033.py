b6ebb63bf85e377788681dd2fe19cff266e3871e
Carlos Leonardo <c.carlos-bp@hotmail.com>
2023-02-24 19:07:24 -0300

    Maior e menor valores

---
+++ PythonExercicios/desafio033.py
@@ -0,0 +1,27 @@
+n1 = int(input('Digite o primeiro valor: '))
+n2 = int(input('Digite o segundo valor: '))
+n3 = int(input('Digite o terceiro valor: '))
+
+# Meu
+'''if n1 < n2 and n1 < n3:
+    menor = n1
+if n2 < n1 and n2 < n3:
+    menor = n2
+if n3 < n1 and n3 < n2:
+    menor = n3
+'''
+# Resolução
+# Verificando o menor:
+menor = n1
+if n2 < n1 and n2 < n3:
+    menor = n2
+if n3 < n1 and n3 < n2:
+    menor = n3
+# Verificando o maior:
+maior = n1
+if n2 > n1 and n2 > n3:
+    maior = n2
+if n3 > n1 and n3 > n2:
+    maior = n3
+print('O menor valor digitado foi {}'.format(menor))
+print('O maior valor digitado foi {}'.format(maior))
