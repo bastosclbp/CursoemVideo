254d5fa6256bf4ef964637569af9fd558b3184c2
Carlos Leonardo <c.carlos-bp@hotmail.com>
2023-02-25 00:31:12 -0300

    Cores no Terminal

--- Mundo1/PythonExercicios/aula011.py
+++ Mundo1/PythonExercicios/aula011.py
@@ -1,6 +1,1 @@
-from rich import print
-from rich.console import Console
-console = Console()
-console.print("Hello", "World!", style="bold red")
-console.print(
-    "Where there is a [bold cyan]Will[/bold cyan] there [u]is[/u] a [i]way[/i].")
+print('\033[1;32mOlá, Mundo!\033[m')
