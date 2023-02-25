0584b5cfe4a34a553a36a3bf663312e22db9f190
Carlos Leonardo <c.carlos-bp@hotmail.com>
2023-02-24 23:00:05 -0300

    Install colorama to ANSI

--- Mundo1/PythonExercicios/desafio035.py
+++ Mundo1/PythonExercicios/desafio035.py
@@ -1,7 +1,9 @@
-seg1 = float(input('Informe o tamanho do primeiro segmento: '))
-seg2 = float(input('Informe o tamanho do segundo segmento: '))
-seg3 = float(input('Informe o tamanho do terceiro segmento: '))
+import colorama
+colorama.init()
+seg1 = float(input('\033[1;30;43m Informe o tamanho do primeiro segmento: '))
+seg2 = float(input('\033[1;30;43m Informe o tamanho do segundo segmento: '))
+seg3 = float(input('\033[1;30;43m Informe o tamanho do terceiro segmento: '))
 if seg2 + seg3 > seg1 and seg1 + seg3 > seg2 and seg1 + seg2 > seg3:
-    print('O triângulo pode ser gerado!')
+    print('\033[1;30;42m O triângulo pode ser gerado!')
 else:
-    print(' \033[0;33;44m O triânglo NÃO pode ser gerado!')
+    print('\033[1;30;41m O triânglo NÃO pode ser gerado!')
